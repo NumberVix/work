446b738a49bc6ba8cbb3cb2a9060ea74d8d2d88b
NumberVix <76859239+NumberVix@users.noreply.github.com>
2021-01-23 06:52:50 +0000

    Update age.js

--- js/age.js
+++ js/age.js
@@ -1,6 +1,6 @@
 let ageEl = document.getElementById("age");
 
 setInterval(() => {
-    let time = (new Date() - new Date(1432512000)) / (1000 * 60 * 60 * 24 * 365.25); // milliseconds per year 1022025600000
+    let time = (new Date() - new Date(1432512000000)) / (1000 * 60 * 60 * 24 * 365.25); // milliseconds per year 1022025600000
     ageEl.innerText = time.toString().substring(0, 12);
 }, 50);
