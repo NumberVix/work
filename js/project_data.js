a19a5b2192c331e4b4b8cb8e585c884a9414b870
NumberVix <76859239+NumberVix@users.noreply.github.com>
2021-01-26 13:25:21 +0000

    Add files via upload

---
+++ js/project_data.js
@@ -0,0 +1,62 @@
+const ProjectData = {
+	"growth_tracker": {
+		title: "Multi Purpose Bot",
+		github: "https://github.com/NumberVix/multipurposevixbot",
+		description: "This was a publicly available bot I developed for fun, and more so to learn some specific NodeJS module documentation to help me progress with the development of more complex bots and automation. I no longer host this specific bot however you can find the source code here: https://github.com/NumberVix/multipurposevixbot"
+	},
+	"dickens": {
+		title: "Mail Bot",
+		github: "https://pastebin.com/ePurcDVn",
+		description: "This was a private project developed specifically to maintain efficency in one large community ran by myself with a team of 3. The purpose of this bot was to allow users to direct message an automated 'Help' user, in where all direct messages received by this 'user' (the bot) would be relayed back to a group for the entire management team to see, meaning all queries can be answered quickly and easily. I cannot release the full source code for this bot, however here is a preview of the code, showing the main index file in the root directory. (https://pastebin.com/ePurcDVn) The full source code is 73 MB, and the directory contains over 14,000 files, with over 1,750 folders.
+	},
+	"minesweeper_b": {
+		title: "Other JS Project",
+		github: "https://pastebin.com/6YX1D0HE",
+		description: "I developed this in Python, as a private script to protect the piracy of some of my work. This is a very small piece of code to protect the code of my Python application, which I sell. A copy of this sales page which I sell the bot can be found at: 'https://numbervix.github.io/VixBotCommands.' This authorization script grabs the user's hardware ID, contacts a web page through a URL, and allows the user access to the application if their HWID is listed. If not, access is denied. A simple yet very effective script. Code: https://pastebin.com/6YX1D0HE "
+	},
+	"dunabase": {
+		title: "Large Online Community Management",
+		github: "https://numbervix.github.io/portfolio/community.PNG",
+		description: "Here is just one example of a large online community I run. It is hard to provide much here, so I've included a full sized screenshot you can view by clicking the button below. This screenshot shows discussion amongst lots of community members and visitors. Here I am giving important information to the community via the Voice Call on the left. You can view this screenshot full size below, and take a look. You can see my name talking to a list of muted individuals on the left side, and also my administrator position on the right, with a list of different ranked members on the right (Which we use to retain engagement) Image: https://numbervix.github.io/portfolio/community.PNG "
+	},
+	"source_mobile": {
+		title: "Analytics",
+		github: "https://numbervix.github.io/portfolio/analytics.JPG",
+		description: "Here is a screenshot of the 24hr analytics page for one of the social outlets I manage. Me and my team use this to consistenly monitor incoming traffic, gains and losses, and make changes to retain activity each day. View full size by clicking here:https://numbervix.github.io/portfolio/analytics.JPG  "
+	},
+	"repertoire": {
+		title: "Content Management + Filtration",
+		github: "https://numbervix.github.io/portfolio/contentfiltering.JPG",
+		description: "Content management is an important part of running any public discussion platform or large social account. Here I am showing a script I made to filter certain frequently posted words and images so they are immediately deleted, or deleted according to the scripts requirements. This helps us manage content without having to view every submitted post and message and moderate them manually. View this full size by clicking here: https://numbervix.github.io/portfolio/contentfiltering.JPG"
+	},
+	"makemehappy": {
+		title: "Sales Page",
+		github: "https://numbervix.github.io/portfolio/vixbotpage.JPG",
+		description: "Here is a website I recently developed to advertise and sell a popular Discord Bot. The website features a command list for the bot, a way to get support, and the option to purchase the bot itself. The website is fairly simple which helps push sales as all information is immediately available including a simple purchase option. Visit the website by clicking here: https://numbervix.github.io/VixBotCommands"
+	},
+	"portfolio": {
+		title: "Portfolio",
+		github: "https://numbervix.github.io/work",
+		description: "The site you are currently on. This website is a one page sitem built using JavaScript, CSS and HTML. Primarily JavaScript."
+	},
+	"minesweeper_f": {
+		title: "Old Portfolio",
+		github: "https://numbervix.github.io/portfolio/",
+		description: "View my previous, outdated portfolio here. Another example of HTML, JavaScript and CSS, for web development. https://numbervix.github.io/portfolio/",
+	},
+	"antislouch": {
+		title: "Photoshop",
+		github: "",
+		description: "",
+	},
+	"reqlang": {
+		title: "ECommerce",
+		github: "",
+		description: "",
+	},
+	"ognes": {
+		title: "GitHub",
+		github: "https://github.com/numbervix",
+		description: "I have lots of experience using GitHub. Visit my portfolio GitHub account here: https://github.com/numbervix",
+	},
+};
